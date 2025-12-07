bbdeb77b87a4bf84557c933a63758c5559124d65
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-12-07 11:28:26 +0100

    add theme

---
+++ zsh-theme.zsh
@@ -0,0 +1,54 @@
+# Custom ZSH theme of Volodymyr Stelmakh
+
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' formats "%%F{blue}[%m%%F{blue}]%%f "
+zstyle ':vcs_info:git*+set-message:*' hooks git-check
++vi-git-check() {
+    local string_truncate() {
+        local LENGTH=$1
+        local ELLIPSIS=$2
+        local STRING=$3
+
+        if (( LENGTH < 0 )); then return 1; fi
+        if (( ${#STRING} > LENGTH )); then
+            if (( LENGTH <= ${#ELLIPSIS} )); then
+                printf '%s' "${STRING[1,$LENGTH]}"
+            else
+                printf '%s' "${STRING[1,$((LENGTH-${#ELLIPSIS}))]}${ELLIPSIS}"
+            fi
+        else
+            printf '%s' "$STRING"
+        fi
+    }
+
+    # - branch name
+    # - or tag name (if tag)
+    # - or short SHA of the commit
+    local REF
+    REF=$(git symbolic-ref --short HEAD 2> /dev/null) \
+    || REF=$(git describe --tags --exact-match HEAD 2> /dev/null) \
+    || REF=$(git rev-parse --short HEAD 2> /dev/null) \
+    || return 0
+
+    REF=$(string_truncate 15 "…" "${REF}")
+    hook_com[misc]+="%F{red}${REF}"
+
+    local STATUS
+    STATUS=$(git status --porcelain 2> /dev/null | tail -n 1)
+    if [[ -n $STATUS ]]; then
+        hook_com[misc]+=" %F{yellow}•" # repository has changes
+    fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook precmd theme_precmd
+theme_precmd () {
+    vcs_info
+}
+
+PROMPT="%B" # text style bold
+PROMPT+="%(?:%F{green}:%F{red})➜ " # previous command status
+PROMPT+="%F{cyan}%~ " # workdir
+PROMPT+='${vcs_info_msg_0_}' # git reference (if git repo)
+PROMPT+="%b%f%u" # reset all text styles
